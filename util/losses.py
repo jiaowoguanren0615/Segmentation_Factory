a192fbc5bc0a3fbe8c241a6d2a4601bf7cc8a9a8
Huangshuqi <1105374939@qq.com>
2024-06-29 22:44:18 +1000

    Upload dataset processing

---
+++ util/losses.py
@@ -0,0 +1,194 @@
+import torch
+from torch import nn, Tensor
+from torch.nn import functional as F
+
+
+__all__ = ['CrossEntropy', 'OhemCrossEntropy', 'Dice', 'FocalLoss']
+
+
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=1, gamma=0, size_average=True, ignore_index=255):
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.ignore_index = ignore_index
+        self.size_average = size_average
+
+    def forward(self, inputs, targets):
+        ce_loss = F.cross_entropy(
+            inputs, targets, reduction='none', ignore_index=self.ignore_index)
+        pt = torch.exp(-ce_loss)
+        focal_loss = self.alpha * (1-pt)**self.gamma * ce_loss
+        if self.size_average:
+            return focal_loss.mean()
+        else:
+            return focal_loss.sum()
+
+
+class CrossEntropy(nn.Module):
+    def __init__(self, ignore_label: int = 255, weight: Tensor = None, aux_weights: list = [1, 0.4, 0.4]) -> None:
+        super().__init__()
+        self.aux_weights = aux_weights
+        self.criterion = nn.CrossEntropyLoss(weight=weight, ignore_index=ignore_label)
+
+    def _forward(self, preds: Tensor, labels: Tensor) -> Tensor:
+        # preds in shape [B, C, H, W] and labels in shape [B, H, W]
+        return self.criterion(preds, labels)
+
+    def forward(self, preds, labels: Tensor) -> Tensor:
+        if isinstance(preds, tuple):
+            return sum([w * self._forward(pred, labels) for (pred, w) in zip(preds, self.aux_weights)])
+        return self._forward(preds, labels)
+
+
+class OhemCrossEntropy(nn.Module):
+    def __init__(self, ignore_label: int = 255, weight: Tensor = None, thresh: float = 0.7, aux_weights: list = [1, 1]) -> None:
+        super().__init__()
+        self.ignore_label = ignore_label
+        self.aux_weights = aux_weights
+        self.thresh = -torch.log(torch.tensor(thresh, dtype=torch.float))
+        self.criterion = nn.CrossEntropyLoss(weight=weight, ignore_index=ignore_label, reduction='none')
+
+    def _forward(self, preds: Tensor, labels: Tensor) -> Tensor:
+        # preds in shape [B, C, H, W] and labels in shape [B, H, W]
+        n_min = labels[labels != self.ignore_label].numel() // 16
+        loss = self.criterion(preds, labels).view(-1)
+        loss_hard = loss[loss > self.thresh]
+
+        if loss_hard.numel() < n_min:
+            loss_hard, _ = loss.topk(n_min)
+
+        return torch.mean(loss_hard)
+
+    def forward(self, preds, labels: Tensor) -> Tensor:
+        if isinstance(preds, tuple):
+            return sum([w * self._forward(pred, labels) for (pred, w) in zip(preds, self.aux_weights)])
+        return self._forward(preds, labels)
+
+
+class Dice(nn.Module):
+    def __init__(self, delta: float = 0.5, aux_weights: list = [1, 0.4, 0.4]):
+        """
+        delta: Controls weight given to FP and FN. This equals to dice score when delta=0.5
+        """
+        super().__init__()
+        self.delta = delta
+        self.aux_weights = aux_weights
+
+    def _forward(self, preds: Tensor, labels: Tensor) -> Tensor:
+        # preds in shape [B, C, H, W] and labels in shape [B, H, W]
+        num_classes = preds.shape[1]
+        labels = F.one_hot(labels, num_classes).permute(0, 3, 1, 2)
+        tp = torch.sum(labels*preds, dim=(2, 3))
+        fn = torch.sum(labels*(1-preds), dim=(2, 3))
+        fp = torch.sum((1-labels)*preds, dim=(2, 3))
+
+        dice_score = (tp + 1e-6) / (tp + self.delta * fn + (1 - self.delta) * fp + 1e-6)
+        dice_score = torch.sum(1 - dice_score, dim=-1)
+
+        dice_score = dice_score / num_classes
+        return dice_score.mean()
+
+    def forward(self, preds, targets: Tensor) -> Tensor:
+        if isinstance(preds, tuple):
+            return sum([w * self._forward(pred, targets) for (pred, w) in zip(preds, self.aux_weights)])
+        return self._forward(preds, targets)
+
+
+class DiceBCELoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(DiceBCELoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        # comment out if your model contains a sigmoid or equivalent activation layer
+        # inputs = torch.nn.functional.sigmoid(inputs)
+
+        # flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+
+        intersection = (inputs * targets).sum()
+        dice_loss = 1 - (2. * intersection + smooth) / (inputs.sum() + targets.sum() + smooth)
+        BCE = torch.nn.functional.binary_cross_entropy_with_logits(inputs, targets, reduction='mean')
+        Dice_BCE = dice_loss + BCE
+        return Dice_BCE
+
+
+
+def get_loss(loss_fn_name: str = 'CrossEntropy', ignore_label: int = 255, cls_weights: Tensor = None):
+    assert loss_fn_name in __all__, f"Unavailable loss function name >> {loss_fn_name}.\nAvailable loss functions: {__all__}"
+    if loss_fn_name == 'Dice':
+        return Dice()
+    return eval(loss_fn_name)(ignore_label, cls_weights)
+
+
+
+def build_target(target: torch.Tensor, num_classes: int = 2, ignore_index: int = -100):
+    """build target for dice coefficient"""
+    dice_target = target.clone()
+    if ignore_index >= 0:
+        ignore_mask = torch.eq(target, ignore_index)
+        dice_target[ignore_mask] = 0
+        # [N, H, W] -> [N, H, W, C]
+        dice_target = nn.functional.one_hot(dice_target, num_classes).float()
+        dice_target[ignore_mask] = ignore_index
+    else:
+        dice_target = nn.functional.one_hot(dice_target, num_classes).float()
+
+    return dice_target.permute(0, 3, 1, 2)
+
+
+def dice_coeff(x: torch.Tensor, target: torch.Tensor, ignore_index: int = -100, epsilon=1e-6):
+    # Average of Dice coefficient for all batches, or for a single mask
+    # compute one class dice_coefficient in every image for each batch
+    d = 0.
+    batch_size = x.shape[0]
+    for i in range(batch_size):
+        x_i = x[i].reshape(-1)
+        t_i = target[i].reshape(-1)
+        if ignore_index >= 0:
+            # 找出mask中不为ignore_index的区域
+            roi_mask = torch.ne(t_i, ignore_index)
+            x_i = x_i[roi_mask]
+            t_i = t_i[roi_mask]
+        inter = torch.dot(x_i, t_i)
+        sets_sum = torch.sum(x_i) + torch.sum(t_i)
+        if sets_sum == 0:
+            sets_sum = 2 * inter
+
+        d += (2 * inter + epsilon) / (sets_sum + epsilon)
+
+    return d / batch_size
+
+
+def multiclass_dice_coeff(x: torch.Tensor, target: torch.Tensor, ignore_index: int = -100, epsilon=1e-6):
+    """Average of Dice coefficient for all classes"""
+    dice = 0.
+    for channel in range(x.shape[1]):
+        dice += dice_coeff(x[:, channel, ...], target[:, channel, ...], ignore_index, epsilon)
+
+    return dice / x.shape[1]
+
+
+def dice_loss(x: torch.Tensor, target: torch.Tensor, multiclass: bool = False, ignore_index: int = -100):
+    # Dice loss (objective to minimize) between 0 and 1
+    x = nn.functional.softmax(x, dim=1)
+    fn = multiclass_dice_coeff if multiclass else dice_coeff
+    return 1 - fn(x, target, ignore_index=ignore_index)
+
+
+
+if __name__ == '__main__':
+
+    def criterion(inputs, target, loss_weight=None, num_classes: int = 2, dice: bool = True, ignore_index: int = -100):
+        loss = nn.functional.cross_entropy(inputs, target, ignore_index=ignore_index, weight=loss_weight)
+        if dice is True:
+            dice_target = build_target(target, num_classes, ignore_index)
+            loss += dice_loss(inputs, dice_target, multiclass=True, ignore_index=ignore_index)
+        return loss
+
+    pred = torch.randint(0, 19, (2, 19, 512, 512), dtype=torch.float)
+    label = torch.randint(0, 19, (2, 512, 512), dtype=torch.long)
+    y = criterion(pred, label, num_classes=19, ignore_index=255)
+    # y = loss_fn(pred, label)
+    print(y)
